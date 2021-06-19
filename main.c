419c3c84ece9779b17fe646861eeab208185f6a9
KivlanRB <kivlantrc@ragtag.moe>
2021-06-20 00:36:29 +0700

    Add ability to modify a movie

--- main.c
+++ main.c
@@ -3,26 +3,29 @@
 #include <conio.h>
 #include <windows.h>
 #include <string.h>
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 
-struct movies{
+typedef struct {
+	int id;
 	char title[40];
 	int status;
 	int rating;
-};
+}movie;
 	
-struct movies mov;
-long int movsize = sizeof(mov);
+movie mov; //Temp var for movie variable
+long int movsize = sizeof(mov); //For file functions (needs size of struct saving)
 
+//Declare funcs
 void addtitle();
 void library();
-void deletetitle();
+void modtitle();
 
 // Main function
 int main(){
 	char another, choice;
     while(1){
     	system("cls");
-    	printf("Main Menu\n(1) Library\n(2) To Watch\n(3) Add a Movie\n(4) Delete a Movie\n(5) Exit\n\n");
+    	printf("Main Menu\n(1) Library\n(2) Add a Movie\n(3) Stats\n(4) Exit\n\n");
 		printf("Enter a choice: ");
 		scanf("%d", &choice);
 		switch(choice){
@@ -30,15 +33,12 @@ int main(){
 				library();
 				break;
 			case 2:
-				
+				addtitle();
 				break;
 			case 3:
-				addtitle();
-				break;	
-			case 4:
 				
 				break;
-			case 5:
+			case 4:
 				exit(0);
 			default:
 				printf("Invalid entry.\n\n");
@@ -50,23 +50,32 @@ int main(){
 }
 
 void library(){
+	char choice;
+	int idChoice;
+	int i = 0;
+	movie arrMovie[100];
 	system("cls");
 	FILE *fp;
 	fp = fopen("LIBRARY.DAT","rb+");
-	if(fp == NULL){
+	if(fp == NULL){ //If no file is found, create file for writing
 		fp = fopen("LIBRARY.DAT","wb+");
-        if(fp == NULL){
+        if(fp == NULL){ //If file still null, something horrible happened.
             printf("Cannot open file!");
             exit(1);
         }
     }
     rewind(fp);
-    printf("Title%-40s\tStatus%-6sRating\n\n", "", "");
+    printf("ID%-5sTitle%-40s\tStatus%-6sRating\n\n", "", "", "");
     while(fread(&mov,movsize,1,fp)==1){
     	/* prints all movie data from file buffer
     	This filters the rating to not display rating when its -1
     	Also replaces int formatting of status to human-readable formatting
+    	It'll also put the entire list of movies into an array for modifying
     	*/
+    	
+    	arrMovie[i] = mov;
+    	i++;
+    	printf("%-5d", mov.id);
     	printf("%-40s\t", mov.title);
     	switch(mov.status){
     		case 1:
@@ -87,14 +96,21 @@ void library(){
 		}
 		printf("\n");
 	}
+	
 	fflush(stdin);
 	fclose(fp);
-	printf("\nPress any key to return to the main menu.");
-    getch();
+	printf("\nPress any key to return to the main menu.\nTo edit an entry, please press the button \"E\"");
+	choice = getch();
+    if(choice == 'e' || choice == 'E'){
+    	printf("\n\nThe ID to modify: ");
+    	scanf("%d", &idChoice);
+    	modtitle(idChoice, arrMovie, i);
+	}
 }
 
 void addtitle(){
 	char temp;
+	int totalMovies;
 	system("cls");
 	FILE *fp;
 	fp = fopen("LIBRARY.DAT","rb+");
@@ -105,7 +121,10 @@ void addtitle(){
             exit(1);
         }
     }
-	fseek(fp, 0, SEEK_END);
+    
+	while(fread(&mov,movsize,1,fp)==1){
+		totalMovies++;
+	}
 	
 	printf("Title: ");
 	scanf("%c", &temp); //Clears buffer from previous inputs
@@ -124,30 +143,69 @@ void addtitle(){
 		case 2:
 		case 3:
 		case 4:
-			printf("Personal Rating (0-100): ");
-			scanf("%d", &mov.rating);
+			while(1){
+				printf("Personal Rating (0-100): ");
+				scanf("%d", &mov.rating);
+				if(mov.rating <= 100 && mov.rating >= 0){
+					break;
+				} else {
+					printf("Rating is outside range!\nPlease retry\n\n");
+				}
+			}
+			
 			break;	
 	}
+	mov.id = totalMovies;
 	fwrite(&mov, movsize, 1, fp);
-	printf("\nSuccessfully recorded. Press any key to return to the main menu.");
+	printf("\nSuccessfully recorded movie with ID %d. Press any key to return to the main menu.", mov.id);
 	
 	fflush(stdin);
 	fclose(fp);
 	getch();
 }
 
-void deletetitle(){
-	char title;
-	system ("cls");
+void modtitle(int id, movie arrMovie[], int arrSize){
 	FILE *fp;
-	fp = fopen("Library.DAT", "rb+");
-	printf("\nEnter a movie title to delete: ");
-	scanf("[^\n]", &title);
-	rewind(fp);
-	if(fp==NULL){
-		printf("\n\tNo Record in File!");
-		printf("\n\t\t\tPress any key to Main Menu..");
-		getch();
-		main();
+	int i,j, choice, temp;
+	
+	system("cls");
+	fp = fopen("LIBRARY.DAT","wb+");
+    if(fp == NULL){
+        printf("Cannot open file!");
+        exit(1);
+    }
+	
+	for(i = 0;i<arrSize;i++){
+		mov = arrMovie[i];
+		if(mov.id == id){
+			printf("(1) Title\n(2) Status\n(3) Rating\n(4) Delete\nInput any other number to go back to menu\n\nChoose what to modify: ");
+			scanf("%d", &choice);
+			switch(choice){
+				case 1:
+					printf("Title: ");
+					scanf("%c", &temp); //Clears buffer from previous inputs
+					scanf("%[^\n]", mov.title);
+					break;
+				case 2:
+					printf("(1) Wishlist\n(2) Dropped\n(3) Watching\n(4) Finished\nInput any other number to go back to menu\n\nChoose a number: ");
+					scanf("%d", &temp);
+					if(temp <= 4 && temp >= 1){
+						mov.status = temp;
+					}
+					break;
+				case 3:
+					printf("Rating: ");
+					scanf("%d", &temp);
+					if(temp <= 100 && temp >= 0){
+						mov.rating = temp;
+					}
+					break;
+			}
+		}
+		fwrite(&mov, movsize, 1, fp);
 	}
+	fflush(stdin);
+	fclose(fp);
 }
+
+
