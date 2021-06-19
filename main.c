72107a556160ed9262ed590983b2cf85cca71d67
nadinealw <nadinealw@gmail.com>
2021-06-19 20:03:20 +0700

    edit w new reference

--- main.c
+++ main.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <windows.h>
 #include <string.h>
-#include <conio.h>
+
+struct movies{
+	char title[40];
+	int rating;
+	int year;
+};
+	
+struct movies mov;
+long int movsize = sizeof(mov);
 
 void addtitle();
-void mainmenu();
-
-// Movie/show data
-struct movies{
-	int index;
-	char title[50];
-	int episodes;
-	int year;
-}Movie;
+void library();
+void deletetitle();
 
 // Main function
 int main(){
-	mainmenu();
-	return 0;
-}
-
-// Main menu
-void mainmenu(void){
-	system("cls"); // clears screen
-	int choice;
-	printf("Main Menu\n(1) Library\n(2) To Watch\n(3) Add a Movie\n(4) Delete a Movie\n(5) Exit\n\n");
-	
-	while(1){
+	char another, choice;
+    while(1){
+    	system("cls");
+    	printf("Main Menu\n(1) Library\n(2) To Watch\n(3) Add a Movie\n(4) Delete a Movie\n(5) Exit\n\n");
 		printf("Enter a choice: ");
 		scanf("%d", &choice);
 		switch(choice){
 			case 1:
-				
+				library();
 				break;
 			case 2:
 				
@@ -49,19 +45,53 @@ void mainmenu(void){
 				break;
 		}
 	}
+	
+	return 0;
 }
 
-// Add title
+void library(){
+	system("cls");
+	FILE *fp;
+	fp = fopen("LIBRARY.DAT","rb+");
+	if(fp == NULL){
+		fp = fopen("LIBRARY.DAT","wb+");
+        if(fp == NULL){
+            printf("Cannot open file!");
+            exit(1);
+        }
+    }
+    rewind(fp);
+    while(fread(&mov,movsize,1,fp)==1){
+    	printf("\n%s %d %d",mov.title,mov.rating,mov.year); /// print the name, age and basic salary
+	}
+	fflush(stdin);
+	fclose(fp);
+    getch();
+}
+
 void addtitle(){
 	system("cls");
-	FILE *sfile;
-	sfile = fopen("ShowLibrary.txt", "a"); // opens txt record file
+	FILE *fp;
+	fp = fopen("LIBRARY.DAT","rb+");
+	if(fp == NULL){
+		fp = fopen("LIBRARY.DAT","wb+");
+        if(fp == NULL){
+            printf("Cannot open file!");
+            exit(1);
+        }
+    }
+	fseek(fp, 0, SEEK_END);
+	
 	printf("Title: ");
-	fflush(stdin);
-	scanf("%s", &Movie.title);
-	fprintf(sfile, "%s\n\n", Movie.title);
-	fclose(sfile);
+	scanf("%s", mov.title);
+	printf("Personal Rating (0-100): ");
+	scanf("%d", &mov.rating);
+	printf("Year: ");
+	scanf("%d", &mov.year);
+	fwrite(&mov, movsize, 1, fp);
 	printf("Successfully recorded. Press any key to return to the main menu.");
+	
+	fflush(stdin);
+	fclose(fp);
 	getch();
-	mainmenu();
 }
