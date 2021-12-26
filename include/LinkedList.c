9053b1d640f58a3a51a4bdcb798bb8215963e787
kivlanRB <kivlantrc@ragtag.moe>
2021-12-26 15:25:38 +0700

    mroe funcs

--- include/LinkedList.c
+++ include/LinkedList.c
@@ -15,10 +15,10 @@ struct node {
 
 void save(Node * head, char *filename){
     FILE *fp;
-    fp = fopen(filename, "w");
+    fp = fopen(filename, "wb");
     if (fp == NULL){
         fprintf(stderr, "Error opening file!");
-        exit(1);
+        return;
     }
 
     Node *current = head;
@@ -80,6 +80,25 @@ void remove_by_id(Node * head, int id){
     }
 }
 
+void sort(Node * head, int key){
+    //TODO: Create insertion sort
+}
+
+void swap(Node * a, Node * b){
+    Node * temp = (Node *) malloc(sizeof(Node));
+
+    //TODO: Create swap func
+}
+
+void copy(Node * a, Node * b){
+    b->id = a->id;
+    b->stock = a->stock;
+    b->year = a->year;
+    strcpy(b->title,a->title);
+    strcpy(b->publisher,a->publisher);
+    strcpy(b->author,a->author);
+}
+
 Node *init(char *title, char *author, char *publisher, int stock, int year){
     Node *head = (Node *) malloc(sizeof(Node));
     head->next = NULL;
@@ -102,7 +121,7 @@ Node *load(char *filename){
     fp = fopen(filename, "r");
     if(fp == NULL){
         fprintf(stderr, "Error loading file!");
-        exit(1);
+        return head;
     }
 
     while(fread(temp, sizeof(Node), 1, fp)){
@@ -122,7 +141,22 @@ Node *load(char *filename){
         strcpy(current->publisher, temp->publisher);
     }
 
+    if(head == NULL){
+        fprintf(stderr, "File has zero data!\n\n");
+    }
+
     fclose(fp);
 
     return head;
+}
+
+int size(Node * node){
+    Node* current = node;
+    int len = 0;
+    while(current != NULL){
+        len++;
+        current = current->next;
+    }
+
+    return len;
 }
--- include/LinkedList.h
+++ include/LinkedList.h
@@ -13,6 +13,14 @@ void print_list(Node * head);
 
 void print_node(Node * node);
 
+void sort(Node * head, int key);
+
+void swap(Node * a, Node * b);
+
+void copy(Node * a, Node * b);
+
+int size(Node * node);
+
 Node *init(char *title, char *author, char *publisher, int stock, int year);
 
 Node *load(char *filename);
