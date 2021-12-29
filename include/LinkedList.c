918b23d5572849f36bc9a4671aa0ebfeb6cb88d6
kivlanRB <kivlantrc@ragtag.moe>
2021-12-29 14:32:54 +0700

    Added sorting, swapping, and node getters.

--- include/LinkedList.c
+++ include/LinkedList.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void save_list(Node * head, char *filename){
     FILE *fp;
@@ -27,7 +28,7 @@ void print_node(Node * node){
 
 void print_list(Node * head){
     Node *current = head;
-    int id;
+    int id = 0;
     printf("ID%-3sTitle%-22sAuthor%-18sPublisher%-15sYear%-4sStock\n", "", "", "", "","");
 
     while(current != NULL){
@@ -70,13 +71,124 @@ void remove_list_by_id(Node * head, int id){
 }
 
 void sort_list(Node * head, int key){
-    //TODO: Create insertion sort
+    if(head == NULL || head->next == NULL){
+        return;
+    }
+
+    char *element = (char *) malloc(sizeof(char));
+    int i,j;
+    unsigned int elm_int;
+    int size = list_size(head);
+
+    switch(key){
+        case 1: //Sort by title
+            for(i = 1; i < size; i++){
+                strcpy(element, get_node(head, i)->title);
+                j = i-1;
+
+                while(j>=0 && strcmp(get_node(head, j)->title, element) > 0){
+                    swap_node(head, j, j+1);
+                    j--;
+                }
+            }
+            break;
+        case 2: //sort by author
+            for(i = 1; i < size; i++){
+                strcpy(element, get_node(head, i)->author);
+                j = i-1;
+
+                while(j>=0 && strcmp(get_node(head, j)->author, element) > 0){
+                    swap_node(head, j, j+1);
+                    j--;
+                }
+            }
+            break;
+        case 3: //Sort by publisher
+            for(i = 1; i < size; i++){
+                strcpy(element, get_node(head, i)->publisher);
+                j = i-1;
+
+                while(j>=0 && strcmp(get_node(head, j)->publisher, element) > 0){
+                    swap_node(head, j, j+1);
+                    j--;
+                }
+            }
+            break;
+        case 4: //Sort by year of publish
+            for(i = 1; i < size; i++){
+                elm_int = get_node(head, i)->year;
+                j = i-1;
+
+                while(j>=0 && get_node(head, j)->year > elm_int){
+                    swap_node(head, j, j+1);
+                    j--;
+                }
+            }
+            break;
+        case 5: //Sort by stock
+            for(i = 1; i < size; i++){
+                elm_int = get_node(head, i)->stock;
+                j = i-1;
+
+                while(j>=0 && get_node(head, j)->stock > elm_int){
+                    swap_node(head, j, j+1);
+                    j--;
+                }
+            }
+            break;
+        default:
+            return;
+    }
 }
 
-void swap_node(Node * a, Node * b){
-    Node * temp = (Node *) malloc(sizeof(Node));
+void swap_node(Node * head, int i, int j){
+    int temp = i;
+    Node *temp_a = (Node *) malloc(sizeof(Node));
+    Node *temp_b = (Node *) malloc(sizeof(Node));
+    Node *current = head;
 
-    //TODO: Create swap func
+    int size = list_size(head);
+    if(size < i || size < j || i == j){
+        return;
+    }
+    if(i > j){
+        i = j;
+        j = temp;
+    }
+
+    while(current != NULL && i > 0){
+        current = current->next;
+        i--;
+        j--;
+    }
+    if(current == NULL){
+        return;
+    } else{
+        copy_node(current, temp_a);
+    }
+    while(current != NULL && j > 0){
+        current = current->next;
+        j--;
+    }
+    if(current == NULL){
+        return;
+    } else{
+        copy_node(current, temp_b);
+        copy_node(temp_a, current);
+    }
+
+    current = head;
+    i = temp;
+
+    while(current != NULL && i > 0){
+        current = current->next;
+        i--;
+    }
+    if(current == NULL){
+        return;
+    } else{
+        copy_node(temp_b, current);
+    }
 }
 
 void copy_node(Node * a, Node * b){
@@ -85,6 +197,17 @@ void copy_node(Node * a, Node * b){
     strcpy(b->title,a->title);
     strcpy(b->publisher,a->publisher);
     strcpy(b->author,a->author);
+}
+
+Node *get_node(Node * head, int index){
+    Node *curr = head;
+
+    while (curr != NULL && index > 0){
+        curr = curr->next;
+        index--;
+    }
+    
+    return curr;
 }
 
 Node *init_list(char *title, char *author, char *publisher, int stock, int year){
